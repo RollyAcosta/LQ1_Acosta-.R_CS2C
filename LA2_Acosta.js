9fbd664565c477cda2fd85b482c3db15302a7779
RollyAcosta <rollyacosta480@gmail.com>
2024-09-25 22:29:58 +0800

    Add files via upload

---
+++ LA2_Acosta.js
@@ -0,0 +1,35 @@
+//Name
+
+var name = "Rolly Siababa Acosta";
+var birthDate = "June 9, 2005";
+var birthPlace = "Iloco Sur District Hospital, Sta lucia, Iloco Sur, Philippines 2712";
+var address = "Calaoa-an, Candon City, Ilocos Sur, Philippines, 2710";
+var course = "Bachelor of Science in Computer Science";
+var year = "2";
+var dreamJob = "Game Developer";
+
+//Classmate1
+
+var nameCM1 = "Aureo Leaño Capellan";
+var birthDateCM1 = "April 11, 2005";
+var birthPlaceCM1 = "Tamurong primero, Candon City, Ilocos Sur, Philippines 2710";
+var addressCM1 = "Tamurong primero, Ilocos Sur, Philippines, 2710";
+var courseCM1 = "Bachelor of Science in Computer Science";
+var yearCM1 = "2";
+var dreamJobCM1 = "Software Engineer";
+ 
+ //Classmate2
+var nameCM2 = "Aj Cortado Lozano";
+var birthDateCM2 = "April 23, 2003";
+var birthPlaceCM2 = "Calaoa-an, Candon City, Iloco Sur, Philippines 2710";
+var addressCM2 = "Calaoa-an, Candon City, Iloco Sur Philippines 2710";
+var courseCM2 = "Bachelor of Science in Computer Science";
+var yearCM2 = "2";
+var dreamJobCM2 = "Software Engineer";
+
+console.log(name.toUpperCase() + " was born " + birthDate + " at " + birthPlace.toUpperCase() + ", and currently living at " + address.toUpperCase()+".");
+console.log(name.toLowerCase() + " is taking up " + course.toLowerCase() + " and dreams to be " + dreamJob + " after graduation.");
+console.log(nameCM1.toUpperCase() + " was born " + birthDateCM1 + " at " + birthPlaceCM1.toUpperCase() + ", and currently living at " + addressCM1.toUpperCase()+".");
+console.log(nameCM1.toLowerCase() + " is taking up " + courseCM1.toLowerCase() + " and dreams to be " + dreamJobCM1 + " after graduation.");
+console.log(nameCM2.toUpperCase() + " was born " + birthDateCM2 + " at " + birthPlaceCM2.toUpperCase() + ", and currently living at " + addressCM2.toUpperCase()+".");
+console.log(nameCM2.toLowerCase() + " is taking up " + courseCM2.toLowerCase() + " and dreams to be " + dreamJobCM2 + " after graduation.");
